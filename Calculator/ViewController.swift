846bec9b6b4e5dcbbbb26054c7eb1d3e56e373c5
yu3san3 <orange12817@yahoo.co.jp>
2023-06-16 12:32:25 +0900

    CollectionViewやLabelを設定した

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = calculatorCollectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var calculatorCollectionView: UICollectionView!
+    @IBOutlet weak var calculatorHeightConstraitnt: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calculatorCollectionView.delegate = self
+        calculatorCollectionView.dataSource = self
+        calculatorCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
     }
 
 
